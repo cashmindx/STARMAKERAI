bd270e5e60577454201321e0c90abf5244e059b8
CASHMIND X <theknightmentor@gmail.com>
2025-07-10 02:04:36 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+document.getElementById('uploadBtn').addEventListener('click', () => {
+  alert('Upload Photo feature coming soon!');
+});
+
+document.getElementById('recordBtn').addEventListener('click', () => {
+  alert('Record Voice feature coming soon!');
+});
+
+document.getElementById('chooseBtn').addEventListener('click', () => {
+  alert('Choose Movie feature coming soon!');
+});
+
+document.getElementById('getStartedBtn').addEventListener('click', () => {
+  alert('Get Started! Redirecting to onboarding...');
+  // Example: window.location.href = '/onboarding.html';
+});
